ff159e6e700213c5b4e1cdadf5690000cba566e3
João Pedro <jp.nascimento505@gmail.com>
2019-07-15 15:47:43 -0300

    adicaoDoProjeto

---
+++ IndicadorCerveja/src/main/java/edu/ifa/IndicadorCerveja/IndicadorCervejaApplication.java
@@ -0,0 +1,13 @@
+package edu.ifa.IndicadorCerveja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndicadorCervejaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndicadorCervejaApplication.class, args);
+	}
+
+}
---
+++ IndicadorCerveja/src/test/java/edu/ifa/IndicadorCerveja/IndicadorCervejaApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.ifa.IndicadorCerveja;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IndicadorCervejaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
